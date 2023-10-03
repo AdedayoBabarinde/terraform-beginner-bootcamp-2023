b6418a879af907cfd5f20393b8ed087e90bc9ed4
Adedayo Babarinde <dbabarinde@gmail.com>
2023-10-03 00:02:12 +0000

    #23 buckets should no longer use random provider

--- providers.tf
+++ providers.tf
@@ -1,23 +1,19 @@
 terraform {
   #backend "remote" {
   #  hostname = "app.terraform.io"
-  #  organization = "realgenie-demo"
+  #  organization = "ExamPro"
 
   #  workspaces {
   #    name = "terra-house-1"
   #  }
   #}
   #cloud {
-     #organization = "realgenie-demo"
-     #workspaces {
-       #name = "terra-house-1"
-    #}
+  #  organization = "ExamPro"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
   #}
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source = "hashicorp/aws"
       version = "5.16.2"
